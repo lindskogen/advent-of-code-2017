426ee7fa0ca0432d1cdf4d36724dc4ec02a9063d
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-12-02 13:32:02 +0100

    Add solution for day 2 part 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,29 +1,31 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+
+fn sort_tuple<'a>(n1: &'a u32, n2: &'a u32) -> (&'a u32, &'a u32) {
+    if n1 > n2 { (n1, n2) } else { (n2, n1) }
+}
+
 fn checksum(input: &str) -> u32 {
     let strings = input.split_whitespace();
     let nbrs: Vec<u32> = strings.map(|c| c.parse::<u32>().unwrap()).collect();
 
-    let mut min = u32::max_value();
-    let mut max = u32::min_value();
+    for n in &nbrs {
+        for m in &nbrs {
+            let (big, small) = sort_tuple(n, m);
+            if big != small && big % small == 0 {
+                return big / small;
+            }
 
-    for n in nbrs {
-        if n > max {
-            max = n;
-        }
-        if n < min {
-            min = n;
         }
     }
-
-    max - min
+    return 0;
 }
 
 fn main() {
-    assert_eq!(8, checksum("5 1 9 5"));
-    assert_eq!(4, checksum("7 5 3"));
-    assert_eq!(6, checksum("2 4 6 8"));
+    assert_eq!(4, checksum("5 9 2 8"));
+    assert_eq!(3, checksum("9 4 7 3"));
+    assert_eq!(2, checksum("3 8 6 5"));
 
     let f = File::open("input").expect("file not found");
     let f = BufReader::new(f);
