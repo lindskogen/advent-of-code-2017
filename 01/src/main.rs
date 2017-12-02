3903944b1f31cbae4f387d19b0f832c7e064eae6
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-12-02 12:45:40 +0100

    Move to subdir

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn calc_sum(input: &str) -> u32 {
+fn calc_sum(input: &str, index_delta: usize) -> u32 {
     let chars = input.chars();
     let nbrs: Vec<u32> = chars.map(|c| c.to_digit(10).unwrap()).collect();
 
     let mut sum: u32 = 0;
-    let index_delta = nbrs.len() / 2;
 
     for n in 0..nbrs.len() {
         if nbrs[(n + index_delta) % nbrs.len()] == nbrs[n] {
@@ -14,24 +13,23 @@ fn calc_sum(input: &str) -> u32 {
         }
     }
 
-    println!("{:?}", sum);
-
     sum
 }
 
 fn main() {
-    assert_eq!(6, calc_sum("1212"));
-    assert_eq!(0, calc_sum("1221"));
-    assert_eq!(4, calc_sum("123425"));
-    assert_eq!(12, calc_sum("123123"));
-    assert_eq!(4, calc_sum("12131415"));
+    assert_eq!(6, calc_sum("1212", 2));
+    assert_eq!(0, calc_sum("1221", 2));
+    assert_eq!(4, calc_sum("123425", 3));
+    assert_eq!(12, calc_sum("123123", 3));
+    assert_eq!(4, calc_sum("12131415", 4));
 
     let f = File::open("input").expect("file not found");
     let f = BufReader::new(f);
 
     for line in f.lines() {
         let line = line.expect("Unable to read line");
-        calc_sum(&line[..]);
+        let sum = calc_sum(&line[..], line.len() / 2);
+        println!("Sum for line: {:?}", sum);
     }
 
 
