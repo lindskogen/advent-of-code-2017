8abf6446d5b3e0e5be91cbcb0d5c19047a73786c
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-12-23 22:13:28 +0100

    Solve day 19

---
+++ 19/src/main.rs
@@ -0,0 +1,89 @@
+// use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const PIPE: char = '|';
+
+#[derive(Debug)]
+enum Direction {
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST,
+}
+
+fn main() {
+    let f = File::open("input").expect("file not found");
+    let f = BufReader::new(f);
+
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut direction: Direction = Direction::SOUTH;
+
+
+    let mut maze: Vec<Vec<char>> = Vec::new();
+    let mut word: Vec<char> = Vec::new();
+    let mut steps = 0;
+
+    for line in f.lines() {
+        let line = line.expect("Unable to read line");
+        let line: Vec<char> = line.chars().collect();
+        maze.push(line);
+    }
+
+    for (index, element) in maze[0].iter().enumerate() {
+        if *element == PIPE {
+            x = index;
+            break;
+        }
+    }
+
+    loop {
+        match direction {
+            Direction::SOUTH => {
+                y += 1;
+            }
+            Direction::NORTH => {
+                y -= 1;
+            }
+            Direction::WEST => {
+                x -= 1;
+            }
+            Direction::EAST => {
+                x += 1;
+            }
+        }
+        steps += 1;
+
+        match maze[y][x] {
+            '|' | '-' => {}
+            '+' => {
+                direction = match direction {
+                    Direction::SOUTH | Direction::NORTH => {
+                        if maze[y][x - 1] == ' ' {
+                            Direction::EAST
+                        } else {
+                            Direction::WEST
+                        }
+                    }
+                    Direction::EAST | Direction::WEST => {
+                        if maze[y - 1][x] == ' ' {
+                            Direction::SOUTH
+                        } else {
+                            Direction::NORTH
+                        }
+                    }
+                };
+            }
+            ' ' => {
+                break;
+            }
+            c => {
+                word.push(c);
+            }
+        }
+    }
+    let st: String = word.iter().collect();
+    println!("Word: {}, steps: {}", st, steps);
+
+}
